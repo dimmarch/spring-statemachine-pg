3ee9476aeb7c1d8e2f948b8d7415e7042471346e
Dmitriy Marchuk <dimmarch@gmail.com>
2021-03-28 10:26:31 +0300

    init commit

---
+++ src/main/java/dev/marchuk/statemachine/Application.java
@@ -0,0 +1,13 @@
+package dev.marchuk.statemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marchuk/statemachine/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marchuk.statemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
