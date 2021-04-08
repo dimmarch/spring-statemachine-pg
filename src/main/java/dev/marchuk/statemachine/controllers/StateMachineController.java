f1c2f555f48289fa57a1f1563478607f69857fe8
Dmitriy Marchuk <dimmarch@gmail.com>
2021-04-08 18:48:04 +0300

    improve test controller

--- src/main/java/dev/marchuk/statemachine/controllers/StateMachineController.java
+++ src/main/java/dev/marchuk/statemachine/controllers/StateMachineController.java
@@ -4,9 +4,12 @@ import dev.marchuk.statemachine.domain.Event;
 import dev.marchuk.statemachine.domain.ActivityState;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.StateMachineEventResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,11 +18,17 @@ public class StateMachineController {
 
     private final StateMachine<ActivityState, Event> stateMachine;
 
-    @GetMapping("/api/status")
-    public String status(String event) {
+    @GetMapping("/api/event")
+    public String event(String event) {
         var currentState = stateMachine.getState();
         var transition = Event.valueOf(event);
-        stateMachine.sendEvent(transition);
+        var results = stateMachine.sendEvent(Mono.just(MessageBuilder
+                .withPayload(transition).build())).collectList().block();
+
+        if(results.get(0).getResultType().equals(StateMachineEventResult.ResultType.DENIED)) {
+            return "transition denied";
+        }
+
         var newState = stateMachine.getState();
         return String.format("%s -> %s", currentState.getId().name(), newState.getId().name());
     }
