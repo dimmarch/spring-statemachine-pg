497f888964e7ad52ec6c43d10ff936c472893b87
Dmitriy Marchuk <dimmarch@gmail.com>
2021-04-09 18:48:25 +0300

    add setState method

--- src/main/java/dev/marchuk/statemachine/service/ActivityTransitionService.java
+++ src/main/java/dev/marchuk/statemachine/service/ActivityTransitionService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.StateMachineEventResult;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -17,7 +18,7 @@ public class ActivityTransitionService {
 
     private final StateMachine<ActivityState, Event> stateMachine;
 
-    public String  makeTransition(Event transition) {
+    public String makeTransition(Event transition) {
         var message = MessageBuilder
                 .withPayload(transition).build();
         var currentState = stateMachine.getState();
@@ -25,12 +26,21 @@ public class ActivityTransitionService {
                 .collectList().block();
         var isSucceeded = results.stream()
                 .map(StateMachineEventResult::getResultType)
-                .anyMatch(StateMachineEventResult.ResultType.DENIED::equals);
+                .noneMatch(StateMachineEventResult.ResultType.DENIED::equals);
         if (!isSucceeded) {
             throw new UnsupportedOperationException(transition.name() + " event is denied");
         }
         var newState = stateMachine.getState();
         return String.format("%s -> %s", currentState.getId().name(), newState.getId().name());
+    }
+
+    public void setState(StateMachine<ActivityState, Event> stateMachine, ActivityState state) {
+        stateMachine
+                .getStateMachineAccessor()
+                .doWithAllRegions(access -> {
+                    access.resetStateMachine(new DefaultStateMachineContext<>(state, null, null, null, null));
+                });
+        stateMachine.startReactively().subscribe();
     }
 
 }
